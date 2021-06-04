586276baabcc02b3e23ff140dae8f871d8924a55
jaison2021 <83944235+jaison2021@users.noreply.github.com>
2021-06-04 19:59:21 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"🔗 Get our Channels Links @JLTMEDIAOFFICIAL 🔗..Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
+                text=f"🔗 Get our Channels Link @JLTMEDIAOFFICIAL 🔗..Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
