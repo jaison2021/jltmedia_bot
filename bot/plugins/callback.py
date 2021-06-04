3d199ee3336362900aa9a2231782a5001eecc784
jaison2021 <83944235+jaison2021@users.noreply.github.com>
2021-06-04 18:57:29 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i>🔗 Get our channels link @JLTMEDIAOFFICIAL 🔗 Found</i> <code>{leng}</code> <i>Results For Your Request 😊:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
